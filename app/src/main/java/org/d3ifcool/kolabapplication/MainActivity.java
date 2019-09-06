a0a3180d030cb9ede1f2a4b56f44dce5065e2130
feni febriani <ffeni.ffebriani@gmail.com>
2019-09-06 09:07:41 +0700

    perubahan bait

--- app/src/main/java/org/d3ifcool/kolabapplication/MainActivity.java
+++ app/src/main/java/org/d3ifcool/kolabapplication/MainActivity.java
@@ -10,10 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //FENI GELO PISAN
         //perubahan pertama
         //perubahan kedua
-        //raka gelo
 
         //Todo Ku coba untuk melawan hati
         //Todo Tapi Hampa terasa
